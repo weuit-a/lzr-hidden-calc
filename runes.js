c94f9ee1a1f8d1bfde420b220d60ea50f208e2e4
weuit-a <lainalrblx@gmail.com>
2025-08-08 12:54:11 +0300

    Update runes.js

--- runes.js
+++ runes.js
@@ -27,5 +27,19 @@ export const runes = [
     exponentialAmount: 1e6,
     prefix: "LIMITED",
   },
-  // добавляй остальные руны сюда
+  {
+    id: 3,
+    name: "Odyssey",
+    type: "Limited",
+    baseAmountScientific: 1e109,
+    chance: 1e109,
+    boosts: [
+      { name: "Rune Bulk", multiplier: 1, max: 10_000 },
+      { name: "Rune Bulk", multiplier: 1, max: 50 },
+      { name: "Rune Speed", multiplier: 1, max: 50 },
+    ],
+    exponential: false,
+    prefix: "LIMITED",
+  },
+  // Добавляй остальные руны сюда...
 ];
